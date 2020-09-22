175bc14114c887b29e32dc21dc8915225fe2f0c1
jasmyne-52 <usnunaf@gmail.com>
2020-09-22 07:51:35 -0400

    finshed the test for the first three functions

--- tests/test_katas.py
+++ tests/test_katas.py
@@ -4,13 +4,20 @@ import katas
 
 class TestKatas(unittest.TestCase):
     def test_add(self):
-        self.fail("TODO: Write add unit test")
+        self.assertEqual(katas.add(1, 1), 2)
+        self.assertEqual(katas.add(1, -1), 0)
+        self.assertEqual(katas.add(0, -1), -1)
 
     def test_multiply(self):
-        self.fail("TODO: Write multiply unit test")
+        self.assertEqual(katas.multiply(1, 1), 1)
+        self.assertEqual(katas.multiply(1, -1), -1)
+        self.assertEqual(katas.multiply(0, -1), 0)
 
     def test_power(self):
-        self.fail("TODO: Write power unit test")
+        self.assertEqual(katas.power(1, 1), 1)
+        self.assertEqual(katas.power(2, 2), 4)
+        self.assertEqual(katas.power(0, 1), 0)
+
 
     def test_factorial(self):
         self.fail("TODO: Write factorial unit test")
