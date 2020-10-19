785cfb615218235bd54a264a359e37e4ccc33f3f
jasmyne-52 <usnunaf@gmail.com>
2020-10-19 09:59:35 -0400

    fixed the text for the factorial and fibonacci functions

--- tests/test_katas.py
+++ tests/test_katas.py
@@ -18,13 +18,18 @@ class TestKatas(unittest.TestCase):
         self.assertEqual(katas.power(2, 2), 4)
         self.assertEqual(katas.power(0, 1), 0)
 
-
     def test_factorial(self):
-        self.fail("TODO: Write factorial unit test")
+        x = [1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800,
+             39916800, 479001600, 6227020800, 87178291200]
+        for i, n in enumerate(x):
+            self.assertEqual(katas.factorial(i+1), n)
 
     def test_fibonacci(self):
-        self.fail("TODO: Write fibonacci unit test")
-
+        x = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987,
+             1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393,
+             196418, 317811, 514229]
+        for i, n in enumerate(x):
+            self.assertEqual(katas.fibonacci(i), n)
 
 if __name__ == '__main__':
     unittest.main()
